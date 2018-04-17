c51f8206f0a93d916c2ffaccb7b67089bfae9d6d
Setugekka <fzjie1006@sina.com>
2018-04-17 10:57:17 +0800

    20180417

--- webapp/Controller/main/view.py
+++ webapp/Controller/main/view.py
@@ -47,7 +47,7 @@ def profilephoto():
     if request.method == 'POST':
         f = request.files['file']
         newname = current_user.username + '.jpg'
-        upload_path = os.path.join(os.getcwd(), 'webapp', 'static', 'avatar', newname)  # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        upload_path = os.path.abspath(os.path.join(paths.project_path, 'static', 'avatar', newname))  # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
         f.save(upload_path)
         return render_template('profilephoto.html', current_user=current_user)
     else:
