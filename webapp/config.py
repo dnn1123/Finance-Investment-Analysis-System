8f3a9b136e2deb98bda0a2b869aedbcf4b8ce169
dnn1123 <603261164@qq.com>
2018-11-22 12:41:40 +0800

    bug

--- webapp/config.py
+++ webapp/config.py
@@ -1,20 +1,24 @@
 # encoding:utf-8
 # 程序配置文件
 import os
+
+
 class Config(object):
     pass
+
 
 class ProdConfig(Config):
     pass
 
+
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:0000@localhost:3306/test'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:0000@116.196.90.212:3306/test'
     SQLALCHEMY_BINDS = {
-    'users_info':        'mysql+pymysql://root:0000@localhost:3306/users_info',
-        'quant':          'mysql+pymysql://root:0000@localhost:3306/quant',
-        'my_message': 'mysql+pymysql://root:0000@localhost:3306/my_message',
-        'cns_stock': 'mysql+pymysql://root:0000@localhost:3306/cns_stock',
+        'users_info': 'mysql+pymysql://root:0000@116.196.90.212:3306/users_info',
+        'quant': 'mysql+pymysql://root:0000@116.196.90.212:3306/quant',
+        'my_message': 'mysql+pymysql://root:0000@116.196.90.212:3306/my_message',
+        'cns_stock': 'mysql+pymysql://root:0000@116.196.90.212:3306/cns_stock',
     }
     SECRET_KEY = 'you-will-never-guess'
     JOBS = [
@@ -36,13 +40,15 @@ class DevConfig(Config):
     ]
     SCHEDULER_API_ENABLED = True
 
+
 class Api_Server(object):
-    server_url='192.168.0.1'
-    server_port='80'
+    server_url = '192.168.0.1'
+    server_port = '80'
+
     def get_server_address(self):
-        return 'http://'+self.server_url+':'+self.server_port
+        return 'http://' + self.server_url + ':' + self.server_port
 
 
 class paths(object):
-    project_path=os.path.dirname(os.path.realpath(__file__))
-    root_path=os.path.dirname(project_path)
+    project_path = os.path.dirname(os.path.realpath(__file__))
+    root_path = os.path.dirname(project_path)
